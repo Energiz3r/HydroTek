b40094ca63a90d0813a0907d5b0878175fe31cbb
Tim Eastwood <tim.eastwood@hotmail.com>
2019-05-01 07:18:55 +1000

    PCB Updated

---
+++ pcb/rename.bat
@@ -0,0 +1,15 @@
+del drillnpt
+del pcb.cad
+del XYRS.csv
+del README.TXT
+del sp1
+del sp2
+ren drill drill.TXT
+ren l1m l1m.GTL
+ren l2m l2m.GBL
+ren outline outline.GML
+ren sm1 sm1.GTS
+ren sm2 sm2.GBS
+ren ss1 ss1.GTO
+ren ss2 ss2.GBO
+pause
